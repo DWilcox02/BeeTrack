d6b2fb9b48a948cc2c80b008211eb923d53a8e58
Daniel Wilcox <danielgwilcox@gmail.com>
2025-02-13 18:41:37 +0000

    Correct tapir vs bootstapir checkpoints

--- init.sh
+++ init.sh
@@ -1,7 +1,2 @@
-wget -P checkpoints/bootstapir https://storage.googleapis.com/dm-tapnet/tapir_checkpoint_panning.npy
-wget -P checkpoints/tapir https://storage.googleapis.com/dm-tapnet/bootstap/bootstapir_checkpoint_v2.npy
-git submodule add https://github.com/deepmind/tapnet
-cd tapnet
-pip install .
-cd ..
-pip install dm-haiku jax mediapy numpy matplotlib tqdm tensorflow
+wget -P checkpoints/tapir https://storage.googleapis.com/dm-tapnet/tapir_checkpoint_panning.npy
+wget -P checkpoints/bootstapir https://storage.googleapis.com/dm-tapnet/bootstap/bootstapir_checkpoint_v2.npy
