1063eb54fdb1ff0a3200a65dceb815714ebead3d
Daniel Wilcox <danielgwilcox@gmail.com>
2025-02-13 19:08:40 +0000

    Fix BeeTrack typo

--- scripts/DoC_gpu_submission.sh
+++ scripts/DoC_gpu_submission.sh
@@ -2,7 +2,7 @@
 #SBATCH --gres=gpu:1
 #SBATCH --mail-type=ALL # required to send email notifcations
 #SBATCH --mail-user=dgw22 # required to send email notifcations
-export PATH=/vol/bitbucket/dgw22/BeeTracker/venv/bin/:$PATH
+export PATH=/vol/bitbucket/dgw22/BeeTrack/venv/bin/:$PATH
 # the above path could also point to a miniconda install
 # if using miniconda, uncomment the below line
 # source ~/.bashrc
