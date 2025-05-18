e84e3b9576b3d6a31bc5dba6a2b9b37cf5f6f2ae
Daniel Wilcox <danielgwilcox@gmail.com>
2025-05-18 20:56:52 +0100

    feat(incremental-nn): :sparkles: Accumulate previous points / predictions

--- src/backend/models/query_point_prediction_model.py
+++ src/backend/models/query_point_prediction_model.py
@@ -12,6 +12,7 @@ class QueryPointPredictionModel():
     def __init__(self):
         self.model = CenterPredictorModel()
         self.optimizer = torch.optim.Adam(self.model.parameters(), lr=1e-3)
+        self.batches = []
 
     def predict(
             self,
@@ -40,12 +41,12 @@ class QueryPointPredictionModel():
         inliers_rotation: tuple[np.ndarray, float],
         true_query_point: np.ndarray,
         initial_positions: np.ndarray,
-        num_steps: int = 500
+        num_steps: int = 100
     ):
         self.model.train()
         
         final_positions = predicted_point_cloud.cloud_points
-        inputs, mask, target = self.prepare_sample(
+        self.prepare_sample(
             final_positions, 
             initial_positions, 
             inliers_rotation, 
@@ -54,15 +55,22 @@ class QueryPointPredictionModel():
         
         total_loss = 0
         for _ in range(num_steps):
-            pred = self.model(inputs, mask)
-            loss = F.mse_loss(pred, target)
-            
-            self.optimizer.zero_grad()
-            loss.backward()
-            self.optimizer.step()
-            total_loss += loss.item()
+            for batch in self.batches:
+                inputs = torch.tensor(batch["input_feats"], dtype=torch.float32).unsqueeze(0)
+                masks = torch.tensor(batch["inliers_mask"], dtype=torch.bool).unsqueeze(0)
+                targets = torch.tensor(batch["target"], dtype=torch.float32).unsqueeze(0)
+
+                pred = self.model(inputs, masks)
+
+                loss = F.mse_loss(pred, targets)
+
+                self.optimizer.zero_grad()
+                loss.backward()
+                self.optimizer.step()
+                total_loss += loss.item()
         
-        return total_loss / num_steps
+        return total_loss / (num_steps * len(self.batches))
+    
     
     def prepare_prediction(
             self, 
@@ -114,8 +122,10 @@ class QueryPointPredictionModel():
         # Feature per point: [x0, y0, dx, dy]
         input_feats = np.concatenate([init, motion], axis=1)  # N x 4
 
-        return (
-            torch.tensor(input_feats, dtype=torch.float32).unsqueeze(0),       # 1 x N x 4
-            torch.tensor(inliers_mask.squeeze(), dtype=torch.bool).unsqueeze(0),  # 1 x N
-            torch.tensor(true_query_point, dtype=torch.float32).unsqueeze(0)  # 1 x 2
-        )
+        current_batch = {
+            "input_feats": input_feats,
+            "inliers_mask": inliers_mask.squeeze(),
+            "target": true_query_point
+        }
+
+        self.batches.append(current_batch)
