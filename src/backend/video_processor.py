3c4ca9cb093cb1f4d20c52c491ebd0f7f150ee9e
Daniel Wilcox <danielgwilcox@gmail.com>
2025-05-24 19:09:20 +0100

    feat(parameters): :sparkles: Smoothing alpha implementation

--- src/backend/video_processor.py
+++ src/backend/video_processor.py
@@ -355,9 +355,10 @@ class VideoProcessor():
             
             for k in range(diff):
                 if k < half:
-                    interpolated_weight = -1/(diff - 1)*k + 1
+                    interpolated_weight = (-2/(diff - 1)*k + 1)**self.smoothing_alpha
                 else:
-                    interpolated_weight =  1/(diff - 1)*k
+                    interpolated_weight =  (2/(diff - 1)*k - 1)**self.smoothing_alpha
+                print(f"Interpolated weight for {k}: {interpolated_weight}")
                 
                 raw_weight = 1 - interpolated_weight
                 smoothed_tracks.append(interpolated_weight * interpolated_tracks[k] + raw_weight * raw_mean_tracks[k])
